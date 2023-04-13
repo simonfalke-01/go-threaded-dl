8ec3f13df4eccda21cfb0ecacf829c33c089aca0
simonfalke <ziqibrandonli@gmail.com>
2023-04-13 21:50:38 +0800

    Fix install.sh error.

--- install.sh
+++ install.sh
@@ -3,4 +3,6 @@
 # Download binary
 curl -L "https://github.com/simonfalke-01/go-threaded-dl/releases/download/v0.0.1/gtd" -o /usr/local/bin/gtd
 
+chmod +x /usr/local/bin/gtd
+
 echo "Installed gtd to /usr/local/bin/gtd."
