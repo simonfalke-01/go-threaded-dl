a57facf7275cab35db8b4e3a247c1aaaef90824d
simonfalke <ziqibrandonli@gmail.com>
2023-04-13 22:03:10 +0800

    Fix install.sh for Linux

--- install.sh
+++ install.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
-# Download binary
-curl -L "https://github.com/simonfalke-01/go-threaded-dl/releases/download/v0.0.1/gtd" -o /usr/local/bin/gtd
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+  curl -L "https://github.com/simonfalke-01/go-threaded-dl/releases/download/v0.0.1/gtd-linux" -o /usr/local/bin/gtd
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+  curl -L "https://github.com/simonfalke-01/go-threaded-dl/releases/download/v0.0.1/gtd-darwin" -o /usr/local/bin/gtd
+fi
 
 chmod +x /usr/local/bin/gtd
 
