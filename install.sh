2ee8c68572059d4c935f844ebfa009f1e4147cac
simonfalke <ziqibrandonli@gmail.com>
2023-04-15 12:52:27 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,15 +6,20 @@ elif [[ "$OSTYPE" == "darwin"* ]]; then
   curl -L "https://github.com/simonfalke-01/go-threaded-dl/releases/download/v1.0.0/gdl-darwin" -o /usr/local/bin/gdl
 fi
 
-if [[ -f "/usr/local/bin/gtd" ]]; then
-  read -p "A legacy version of go-threaded-dl is found at /usr/local/bin/gtd, do you want to remove it? [y/N] " -n 1 -r
-  echo
-  if [[ ! $REPLY =~ ^[Yy]$ ]]; then
-    echo "Installation aborted."
-    exit 1
-  fi
-fi
-
 chmod +x /usr/local/bin/gdl
 
-echo "Installed gtd to /usr/local/bin/gdl."
+if [ -e /usr/local/bin/gtd ]; then
+    read -p "A legacy version of go-threaded-dl is found at /usr/local/bin/gtd, do you want to remove it? [Y/n] " -r answer
+    if [[ "$answer" =~ ^[Yy]$ ]]; then
+        rm /usr/local/bin/gtd
+    elif [[ "$answer" =~ ^[Nn]$ ]]; then
+        echo "/usr/local/bin/gtd will not be removed."
+    elif [ -z "$answer" ]; then
+        rm /usr/local/bin/gtd
+    else
+        echo "Aborted."
+        exit 1
+    fi
+fi
+
+echo "Installed gdl to /usr/local/bin/gdl."
